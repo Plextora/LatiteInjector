bf8604eee666a8943314fc13e228a4161fad3527
Plextora <71889427+Plextora@users.noreply.github.com>
2023-01-21 14:36:22 -0500

    Added launch button (only launches vanilla mc for now)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 
 namespace LatiteInjector;
@@ -16,4 +17,18 @@ public partial class MainWindow
     private void CloseButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
     private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
     private void WindowToolbar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+
+    private void LaunchButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (Process.GetProcessesByName("Minecaft.Windows").Length != 0) return;
+        var process = new Process();
+        var startInfo = new ProcessStartInfo
+        {
+            WindowStyle = ProcessWindowStyle.Normal,
+            FileName = "explorer.exe",
+            Arguments = "shell:appsFolder\\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App"
+        };
+        process.StartInfo = startInfo;
+        process.Start();
+    }
 }
