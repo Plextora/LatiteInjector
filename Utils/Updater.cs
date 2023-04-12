a8c262b2d7dce4a4a0e552e1b38e806c384bf512
Plextora <71889427+Plextora@users.noreply.github.com>
2023-04-12 17:46:00 -0400

    Updated injector to version 10

--- Utils/Updater.cs
+++ Utils/Updater.cs
@@ -9,7 +9,7 @@ namespace LatiteInjector.Utils;
 
 public static class Updater
 {
-    public const string InjectorCurrentVersion = "9";
+    public const string InjectorCurrentVersion = "10";
     private const string INJECTOR_VERSION_URL =
         "https://raw.githubusercontent.com/Imrglop/Latite-Releases/main/launcher_version";
     private const string DLL_VERSION_URL =
