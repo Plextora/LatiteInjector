03cf7992ebb2779691a2773d5bd96a3964d51766
Plextora <71889427+Plextora@users.noreply.github.com>
2024-05-24 16:04:51 -0400

    band-aid fix no longer needed! hurray!

--- Utils/IsExternalInit.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit { }
-}
-// https://stackoverflow.com/a/64749403
-// this is one of the worst band-aid fixes i've done recently
