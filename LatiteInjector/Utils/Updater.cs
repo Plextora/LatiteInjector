570e8264ef15a3705169102d358cc480d2dcbc3e
Plextora <71889427+Plextora@users.noreply.github.com>
2024-06-12 21:44:53 -0400

    Update injector version to 17

--- LatiteInjector/Utils/Updater.cs
+++ LatiteInjector/Utils/Updater.cs
@@ -11,7 +11,7 @@ namespace LatiteInjector.Utils;
 
 public static class Updater
 {
-    public const string InjectorCurrentVersion = "16";
+    public const string InjectorCurrentVersion = "17";
 
     private static readonly Uri InjectorVersionUrl =
         new("https://raw.githubusercontent.com/Imrglop/Latite-Releases/main/launcher_version");
