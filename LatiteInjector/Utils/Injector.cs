def77a8c1e5f3f8f4384173aa0051a936733d02e
Plextora <71889427+Plextora@users.noreply.github.com>
2024-07-20 16:10:17 -0400

    -1000 silliness!

--- LatiteInjector/Utils/Injector.cs
+++ LatiteInjector/Utils/Injector.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 using System.Security.AccessControl;
 using System.Security.Principal;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
